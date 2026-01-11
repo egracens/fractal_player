1bc351876bf1d56a21b73e0efbb867a6ffde866d
Vlad Petrovsky <egrace@yandex.ru>
2026-01-12 00:06:14 +0300

    Reorganize callback modules

--- src/ui/fractal_callbacks/common/vertex.rs
+++ src/ui/fractal_callbacks/common/vertex.rs
@@ -1,5 +1,3 @@
-use wgpu::util::DeviceExt;
-
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct Vertex {
@@ -8,7 +6,7 @@ pub struct Vertex {
 }
 
 impl Vertex {
-    pub const ATTRIBS: [wgpu::VertexAttribute; 2] =
+    pub const ATTRIBUTES: [wgpu::VertexAttribute; 2] =
         wgpu::vertex_attr_array![0 => Float32x2, 1 => Float32x3];
 
     pub fn desc() -> wgpu::VertexBufferLayout<'static> {
@@ -17,7 +15,8 @@ impl Vertex {
         wgpu::VertexBufferLayout {
             array_stride: mem::size_of::<Self>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &Self::ATTRIBS,
+            attributes: &Self::ATTRIBUTES,
         }
     }
 }
+
--- src/ui/fractal_callbacks/mod.rs
+++ src/ui/fractal_callbacks/mod.rs
@@ -2,3 +2,4 @@ pub mod common;
 pub mod triangle;
 
 pub use triangle::*;
+
